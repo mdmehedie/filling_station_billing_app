f3b52c31bd2ad2940eb0d5b2ef28a617c35b8ee2
Mehedi Hasan <mehedicse04@gmail.com>
2026-06-06 13:11:30 +0600

    change migrations

--- database/migrations/2026_05_07_183050_add_previous_due_and_previous_paid_to_organizations_table.php
+++ database/migrations/2026_05_07_183050_add_previous_due_and_previous_paid_to_organizations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PaymentMethodTypeEnums;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,13 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('organizations', function (Blueprint $table) {
-            $table->decimal('previous_due', 15, 2)->default(0)->after('security_money');
-            $table->decimal('previous_paid', 15, 2)->default(0)->after('previous_due');
-        });
+//        Schema::table('organizations', function (Blueprint $table) {
+//            $table->decimal('previous_due', 15, 2)->default(0)->after('security_money');
+//            $table->decimal('previous_paid', 15, 2)->default(0)->after('previous_due');
+//        });
 
         Schema::table('payments', function (Blueprint $table) {
-            $table->renameColumn('type', 'method');
+            $table->enum('method', PaymentMethodTypeEnums::getValueAsArray());
             $table->boolean('is_deleted')->default(false)->after('proof');
         });
 
